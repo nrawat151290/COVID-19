663fb6cb525748f80f4a5b4bdb3c64a89912a405
nirawat <nipun.rawat@gartner.com>
2020-04-08 21:42:58 +0530

    Updates

---
+++ precache-manifest.1cd86053e12ed4208dad603122caa1e5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "24c5c7d50377950b2588b68a88a5cf1d",
+    "url": "/COVID-19/index.html"
+  },
+  {
+    "revision": "498aff6ff0c275f2047f",
+    "url": "/COVID-19/static/css/main.55d536c3.chunk.css"
+  },
+  {
+    "revision": "2e1dcd4a67272d6c44ce",
+    "url": "/COVID-19/static/js/2.8c57e3e1.chunk.js"
+  },
+  {
+    "revision": "8a35e35e00e763aabcd15e61b0cb2343",
+    "url": "/COVID-19/static/js/2.8c57e3e1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "498aff6ff0c275f2047f",
+    "url": "/COVID-19/static/js/main.ecfcd5ff.chunk.js"
+  },
+  {
+    "revision": "80da2e9c3df50dbd7316",
+    "url": "/COVID-19/static/js/runtime-main.2bc83139.js"
+  }
+]);
