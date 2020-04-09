d17246c57018ca24e33f814006e10c4e17da74de
nirawat <nipun.rawat@gartner.com>
2020-04-09 12:39:42 +0530

    changed to hash router

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
-import { Route, BrowserRouter as Router } from 'react-router-dom';
+import { Route, HashRouter as Router } from 'react-router-dom';
 import App from './App';
 import Districts from './Containers/Districts/districts';
 
 const routes = () => {
   return (
-    <Router>
+    <Router basename="/">
       <Route exact path="/" component={App} />
       <Route exact path="/:state/districts" component={Districts} />
     </Router>
